16587edbf99a338480b8c0358d53dc3e24f973c4
Kristina <crislayn@yahoo.com>
2023-07-05 19:15:04 +0300

    late corrections..

--- src/Components/Pasaulis.jsx
+++ src/Components/Pasaulis.jsx
@@ -8,26 +8,34 @@ function Pasaulis({ list }) {
     <>
       <h4 style={{ color: 'peru' }}>Porine Pasaulio dalis:</h4>
       {list.map((el) =>
-        el.id % 2
-          ? null
-          : [
-              <Namas list={el} color='crimson' />,
-              <Garazas list={el} color='crimson' />,
-              <Narvas list={el} color='crimson' />,
-              <Akvariumas list={el} color='crimson' />,
-            ]
-      )}
+        el.id % 2 ? null
+          : <Namas list={el} color='crimson' key={el.id} />)}
+      -------------------------
+      {list.map((el) =>
+        el.id % 2 ? null
+          : <Garazas list={el} color='crimson' key={el.id} />)}
+      -------------------------
+      {list.map((el) =>
+        el.id % 2 ? null
+          : <Narvas list={el} color='crimson' key={el.id} />)}
+      -------------------------
+      {list.map((el) =>
+        el.id % 2 ? null
+          : <Akvariumas list={el} color='crimson' key={el.id} />)}
+      -------------------------
+
       <h4 style={{ color: 'peru' }}>Neporine pasaulio dalis:</h4>
       {list.map((el) =>
-        el.id % 2
-          ? [
-              <Namas list={el} color='crimson' />,
-              <Garazas list={el} color='crimson' />,
-              <Narvas list={el} color='crimson' />,
-              <Akvariumas list={el} color='crimson' />,
-            ]
-          : null
-      )}
+        el.id % 2 ? <Namas list={el} color='crimson' key={el.id} /> : null)}
+      --------------------------
+      {list.map((el) =>
+        el.id % 2 ? <Garazas list={el} color='crimson' key={el.id} /> : null)}
+      --------------------------
+      {list.map((el) =>
+        el.id % 2 ? <Narvas list={el} color='crimson' key={el.id} /> : null)}
+      --------------------------
+      {list.map((el) =>
+        el.id % 2 ? <Akvariumas list={el} color='crimson' key={el.id} /> : null)}
     </>
   )
 }
